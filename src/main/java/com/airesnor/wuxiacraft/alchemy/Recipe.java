12d7bf964a55a1ac2075595166bbf0d80304d8dc
Aires Noronha Adures Filho <airesnoga@gmail.com>
2020-03-09 11:03:04 -0400

    Recipes for testing

--- src/main/java/com/airesnor/wuxiacraft/alchemy/Recipe.java
+++ src/main/java/com/airesnor/wuxiacraft/alchemy/Recipe.java
@@ -1,24 +1,36 @@
 package com.airesnor.wuxiacraft.alchemy;
 
-import akka.japi.Pair;
+import com.airesnor.wuxiacraft.utils.MathUtils;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe {
 
-	private List<Pair<Integer, Item>> recipeOrder;
+	private List<Triple<Float, Float, Item>> recipeOrder;
 
 	public Recipe() {
 		recipeOrder = new ArrayList<>();
 	}
 
-	public Recipe addRecipeItem(int temperature, Item item) {
-		this.recipeOrder.add(Pair.apply(temperature, item));
+	public Recipe addRecipeItem(float minTemperature, float maxTemperature, Item item) {
+		this.recipeOrder.add(Triple.of(minTemperature, maxTemperature, item));
 		return this;
 	}
 
+	public boolean checkRecipe (List<Pair<Float, Item>> inputs) {
+		boolean isThis = false;
+		for(int i = 0; i< inputs.size(); i++) {
+			Triple<Float, Float, Item> element = recipeOrder.get(i);
+			Pair<Float, Item> input = inputs.get(i);
+			if(input.getRight() == element.getRight()) {
+				isThis = MathUtils.between(input.getLeft(), element.getLeft(), element.getMiddle());
+			}
+		}
+		return isThis;
+	}
 
 }
--- src/main/java/com/airesnor/wuxiacraft/alchemy/Recipes.java
+++ src/main/java/com/airesnor/wuxiacraft/alchemy/Recipes.java
@@ -1,4 +1,23 @@
 package com.airesnor.wuxiacraft.alchemy;
 
+import net.minecraft.item.Item;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recipes {
+
+    public static final List<Recipe> RECIPES = new ArrayList<>();
+
+    public static List<Recipe> getRecipeCandidatesByInput(List<Pair<Float, Item>> input) {
+        List<Recipe> candidates = new ArrayList<>();
+        for(Recipe recipe : RECIPES) {
+            if(recipe.checkRecipe(input)) {
+                candidates.add(recipe);
+            }
+        }
+        return candidates;
+    }
+
 }
