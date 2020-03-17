01dcaa49bfe3779b63666e1ceedcdd479f21fb93
Aires Noronha Adures Filho <airesnoga@gmail.com>
2020-03-17 17:17:11 -0400

    Started making the wandering cultivator mob.

---
+++ src/main/java/com/airesnor/wuxiacraft/entities/mobs/WanderingCultivator.java
@@ -0,0 +1,15 @@
+package com.airesnor.wuxiacraft.entities.mobs;
+
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.World;
+
+public class WanderingCultivator extends EntityCreature {
+
+	public WanderingCultivator(World worldIn) {
+		super(worldIn);
+	}
+
+
+}
