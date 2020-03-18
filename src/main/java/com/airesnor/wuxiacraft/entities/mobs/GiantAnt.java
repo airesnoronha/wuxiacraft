8c04aeaec5ff10a2e316c696bd5a47f7deb23f4b
Aires Noronha Adures Filho <airesnoga@gmail.com>
2020-03-18 09:24:32 -0400

    Added a new mob

---
+++ src/main/java/com/airesnor/wuxiacraft/entities/mobs/GiantAnt.java
@@ -0,0 +1,13 @@
+package com.airesnor.wuxiacraft.entities.mobs;
+
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+
+public class GiantAnt extends EntityMob {
+
+	public GiantAnt(World worldIn) {
+		super(worldIn);
+	}
+
+
+}
