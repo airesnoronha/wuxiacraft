d05f1be41d9082a8756505479e3d543afebdc202
Ano <yoloswag345787@gmail.com>
2020-04-30 20:35:39 +1000

    0.0.8.0: Quick fix 6

--- src/main/java/com/airesnor/wuxiacraft/dimensions/biomes/WuxiaBiomes.java
+++ src/main/java/com/airesnor/wuxiacraft/dimensions/biomes/WuxiaBiomes.java
@@ -25,7 +25,7 @@ public class WuxiaBiomes {
 
         //This makes it so that the overworld gets the biome added then it spawns it
         if(spawnOverworld) {
-            BiomeManager.addBiome(biomeType, new BiomeManager.BiomeEntry(biome, 50));
+            BiomeManager.addBiome(biomeType, new BiomeManager.BiomeEntry(biome, 10));
         }
         BiomeManager.addSpawnBiome(biome);
         System.out.println("The biomes have been added");
