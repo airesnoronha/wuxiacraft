e1e68134e5abf2159f514218eb22ec5952159b30
Ano <yoloswag345787@gmail.com>
2020-08-01 02:27:13 +1000

    0.0.9.0: Minor Fix.

--- decrypt_recipes.sh
+++ decrypt_recipes.sh
@@ -6,5 +6,5 @@ read -p "Enter the password: " PASSWORD
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSWORD" \
---output $THISDIR/src/main/java/com/airesnor/wuxiacraft/alchemy/Recipes.java \
-$THISDIR/src/main/java/com/airesnor/wuxiacraft/alchemy/Recipes.Encrypted
+--output $THISDIR/src/main/java/com/airesnor/wuxiacraft/profession/alchemy/Recipes.java \
+$THISDIR/src/main/java/com/airesnor/wuxiacraft/profession/alchemy/Recipes.Encrypted
