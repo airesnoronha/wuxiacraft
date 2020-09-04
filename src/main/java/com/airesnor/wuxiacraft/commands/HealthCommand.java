85a485b6678d5c8d75c9e1e5969ed07359eedab6
Ano <yoloswag345787@gmail.com>
2020-09-04 13:41:41 +1000

    0.0.9.20: Made it so that now some commands work in console.

--- src/main/java/com/airesnor/wuxiacraft/commands/HealthCommand.java
+++ src/main/java/com/airesnor/wuxiacraft/commands/HealthCommand.java
@@ -37,16 +37,16 @@ public class HealthCommand extends CommandBase {
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		if (sender instanceof EntityPlayerMP) {
-			EntityPlayerMP playerMP = (EntityPlayerMP) sender;
-			if (!playerMP.world.isRemote) {
-				boolean wrongUsage = true;
-				if (args.length == 2) {
-					EntityPlayerMP targetPlayer = server.getPlayerList().getPlayerByUsername(args[1]);
-					if (targetPlayer != null) {
-						if (args[0].equalsIgnoreCase("get")) {
-							float maxHealth = targetPlayer.getMaxHealth();
-							float currentHealth = targetPlayer.getHealth();
+		if (!sender.getEntityWorld().isRemote) {
+			boolean wrongUsage = true;
+			if (args.length == 2) {
+				EntityPlayerMP targetPlayer = server.getPlayerList().getPlayerByUsername(args[1]);
+				if (targetPlayer != null) {
+					if (args[0].equalsIgnoreCase("get")) {
+						float maxHealth = targetPlayer.getMaxHealth();
+						float currentHealth = targetPlayer.getHealth();
+						if (sender instanceof EntityPlayerMP) {
+							EntityPlayerMP playerMP = (EntityPlayerMP) sender;
 							if (targetPlayer.getUniqueID().equals(playerMP.getUniqueID())) {
 								TextComponentString text = new TextComponentString("Your Health:");
 								sender.sendMessage(text);
@@ -60,52 +60,58 @@ public class HealthCommand extends CommandBase {
 								sender.sendMessage(text);
 								wrongUsage = false;
 							}
-						}
-					} else {
-						TextComponentString text = new TextComponentString("Couldn't find player " + args[1]);
-						text.getStyle().setColor(TextFormatting.RED);
-						sender.sendMessage(text);
-						wrongUsage = true;
-					}
-				} else if (args.length == 3) {
-					EntityPlayerMP targetPlayer = server.getPlayerList().getPlayerByUsername(args[1]);
-					float health = 0;
-					if (targetPlayer != null) {
-						if (args[2].equalsIgnoreCase("max")) {
-							try {
-								health = targetPlayer.getMaxHealth();
-							} catch (NullPointerException e) {
-								TextComponentString text = new TextComponentString("Invalid Max Health");
-								sender.sendMessage(text);
-							}
 						} else {
-							health = Float.parseFloat(args[2]);
-						}
-						if (args[0].equalsIgnoreCase("set")) {
-							targetPlayer.setHealth(health);
-							TextComponentString text = new TextComponentString(String.format("%s's health set to %.1f", targetPlayer.getName(), health));
+							TextComponentString text = new TextComponentString(String.format("%s's Health:", targetPlayer.getName()));
 							sender.sendMessage(text);
-							wrongUsage = false;
-						} else if (args[0].equalsIgnoreCase("heal")) {
-							targetPlayer.heal(health);
-							TextComponentString text = new TextComponentString(String.format("%s's health healed by %.1f", targetPlayer.getName(), health));
+							text = new TextComponentString(String.format("%.1f / %.1f", currentHealth, maxHealth));
 							sender.sendMessage(text);
 							wrongUsage = false;
 						}
-					} else {
-						TextComponentString text = new TextComponentString("Couldn't find player " + args[1]);
-						text.getStyle().setColor(TextFormatting.RED);
-						sender.sendMessage(text);
-						wrongUsage = true;
 					}
-				}
-				if (wrongUsage) {
-					TextComponentString text = new TextComponentString("Invalid arguments, use /health [set:heal] <player> <amount> or /health get <player>");
+				} else {
+					TextComponentString text = new TextComponentString("Couldn't find player " + args[1]);
 					text.getStyle().setColor(TextFormatting.RED);
 					sender.sendMessage(text);
+					wrongUsage = true;
+				}
+			} else if (args.length == 3) {
+				EntityPlayerMP targetPlayer = server.getPlayerList().getPlayerByUsername(args[1]);
+				float health = 0;
+				if (targetPlayer != null) {
+					if (args[2].equalsIgnoreCase("max")) {
+						try {
+							health = targetPlayer.getMaxHealth();
+						} catch (NullPointerException e) {
+							TextComponentString text = new TextComponentString("Invalid Max Health");
+							sender.sendMessage(text);
+						}
+					} else {
+						health = Float.parseFloat(args[2]);
+					}
+					if (args[0].equalsIgnoreCase("set")) {
+						targetPlayer.setHealth(health);
+						TextComponentString text = new TextComponentString(String.format("%s's health set to %.1f", targetPlayer.getName(), health));
+						sender.sendMessage(text);
+						wrongUsage = false;
+					} else if (args[0].equalsIgnoreCase("heal")) {
+						targetPlayer.heal(health);
+						TextComponentString text = new TextComponentString(String.format("%s's health healed by %.1f", targetPlayer.getName(), health));
+						sender.sendMessage(text);
+						wrongUsage = false;
+					}
+				} else {
+					TextComponentString text = new TextComponentString("Couldn't find player " + args[1]);
+					text.getStyle().setColor(TextFormatting.RED);
+					sender.sendMessage(text);
+					wrongUsage = true;
 				}
 			}
-		} else throw new CommandException("Not used correctly!");
+			if (wrongUsage) {
+				TextComponentString text = new TextComponentString("Invalid arguments, use /health [set:heal] <player> <amount> or /health get <player>");
+				text.getStyle().setColor(TextFormatting.RED);
+				sender.sendMessage(text);
+			}
+		}
 	}
 
 	@Override
