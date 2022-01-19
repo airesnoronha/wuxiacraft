e706148009449272f947fa3d93e1d4928c0ddb23
Aires Noronha Adures Filho <airesnoga@gmail.com>
2022-01-19 11:28:51 -0400

    Changing version to 1.18.1, creating everything anew, first commit

--- src/main/java/com/airesnor/wuxiacraft/cultivation/skills/ISkillAction.java
+++
@@ -1,9 +0,0 @@
-package com.airesnor.wuxiacraft.cultivation.skills;
-
-import net.minecraft.entity.EntityLivingBase;
-
-public interface ISkillAction {
-
-	boolean activate(EntityLivingBase actor);
-
-}
