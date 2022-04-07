318a8e528b404a1d6baa7415e3ef78e8a88e38f0
Aires Noronha Adures Filho <airesnoga@gmail.com>
2022-04-07 15:56:13 -0400

    Removed the unused

--- src/main/java/com/lazydragonstudios/wuxiacraft/init/RegistryEvents.java
+++ src/main/java/com/lazydragonstudios/wuxiacraft/init/RegistryEvents.java
@@ -4,14 +4,11 @@ package com.lazydragonstudios.wuxiacraft.init;
 import com.lazydragonstudios.wuxiacraft.container.InscriberMenu;
 import com.lazydragonstudios.wuxiacraft.container.IntrospectionMenu;
 import com.lazydragonstudios.wuxiacraft.container.RunemakingMenu;
-import com.lazydragonstudios.wuxiacraft.crafting.RuneMakingRecipe;
 import com.lazydragonstudios.wuxiacraft.cultivation.CultivationRealm;
 import com.lazydragonstudios.wuxiacraft.cultivation.CultivationStage;
-import com.lazydragonstudios.wuxiacraft.cultivation.skills.aspects.SkillAspect;
 import com.lazydragonstudios.wuxiacraft.cultivation.skills.aspects.SkillAspectType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.event.RegistryEvent;
@@ -81,4 +78,5 @@ public class RegistryEvents {
 		event.getRegistry().register(InscriberMenu.registryType);
 		event.getRegistry().register(RunemakingMenu.registryType);
 	}
+
 }
